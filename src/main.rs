f9783fbde8f9ea5bccf42776e56d0e2886709f17
Kazunari Tanaka <tzskp1@gmail.com>
2021-07-11 15:47:47 +0900

    change dir str

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::path::PathBuf;
 
 async fn index(req: HttpRequest) -> Result<NamedFile> {
-    Ok(NamedFile::open("./front/build/index.html")?)
+    Ok(NamedFile::open("./build/index.html")?)
 }
 
 #[actix_web::main]
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
         .expect("PORT must be a number");
     HttpServer::new(|| App::new()
                     .route("/", web::get().to(index))
-                    .service(Files::new("/", "./front/build").prefer_utf8(true)))
+                    .service(Files::new("/", "./build").prefer_utf8(true)))
         .bind(("0.0.0.0", port))?
         .run()
         .await
